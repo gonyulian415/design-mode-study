3c070b8d62191a59935c2b4f08ef0c292bcaa9ff
alan <415400755@qq.com>
2023-02-22 14:32:40 +0800

    project init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="wrapper">
+    <div class="nav">
+      <RouterLink to="/publish">发布订阅者模式</RouterLink>
+      <RouterLink to="/about">装饰器模式</RouterLink>
+    </div>
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+
+}
+
+.nav {
+  display: flex;
+  flex-direction: column;
+}
+</style>
---
+++ src/views/Publish.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div id="publishWrapper">
+        <h1>发布订阅者模式</h1>
+    </div>
+</template>
+
+<style scoped>
+#publishWrapper {
+    /* border: 1px solid red; */
+}
+</style>
